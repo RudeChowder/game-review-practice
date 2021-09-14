b6f0afb6d381df8c0381bdbd8d5afbd26f164f4b
Stephen Crane <scrane88@gmail.com>
2021-09-14 14:57:37 -0400

    add React client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import GamesPage from './Games/GamesPage';
+
+
+function App() {
+  return (
+    <GamesPage/>
+  )
+}
+
+export default App;
---
+++ client/src/Games/GamesPage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const GamesPage = () => {
+
+  return(
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default GamesPage
